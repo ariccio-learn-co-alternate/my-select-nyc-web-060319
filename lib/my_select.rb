8cf8c22cf228095f863a821324c14a2f285f46f2
Alexander Riccio <test35965@gmail.com>
2019-05-20 17:37:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
 def my_select(collection)
  new_collection = []
- collection.each do |elem|
+ collection.each_with_index do |elem, index|
    new_elem = yield(elem)
    if new_elem
-     new_collection << new_elem 
+     new_collection << collection[index]
   end
  end
  return new_collection
