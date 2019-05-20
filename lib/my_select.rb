193b75dbfd32f602b031f7c90c356c5537bfccd9
Alexander Riccio <test35965@gmail.com>
2019-05-20 17:36:03 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,8 @@
 def my_select(collection)
  new_collection = []
  collection.each do |elem|
-   new_elem = yield(elem.even?)
-   if new_elem
+   new_elem = yield(elem)
+   if new_elem.even?
      new_collection << new_elem 
   end
  end
