ab3076f050dbd902133a49da1608ec53a4eddbb4
Alexander Riccio <test35965@gmail.com>
2019-05-20 17:37:18 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ new_collection = []
+ collection.each_with_index do |elem, index|
+   new_elem = yield(elem)
+   if new_elem
+     new_collection << collection[index]
+  end
+ end
+ return new_collection
 end
